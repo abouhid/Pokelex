9cfce4d9a6e6068784e03e2e7e7b6136504ade8f
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-01-26 22:09:16 -0300

    managed to useEffect the evolution-chain

--- src/Context.js
+++ src/Context.js
@@ -6,7 +6,9 @@ const Context = React.createContext();
 
 function ContextProvider({ children }) {
   const [query, setQuery] = useState("");
-  const [{ data, isLoading, isError }, doFetch] = FetchData();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [{ data }, doFetch] = FetchData();
   const getNum = (url) => {
     const el = url.replace(/.*\D(?=\d)|\D+$/g, "");
     return el;
@@ -18,7 +20,9 @@ function ContextProvider({ children }) {
         setQuery,
         data,
         isError,
+        setIsError,
         isLoading,
+        setIsLoading,
         doFetch,
         FetchData,
         getNum,
--- src/pages/PokeDetails.js
+++ src/pages/PokeDetails.js
@@ -1,30 +1,67 @@
 /*eslint-disable */
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import FetchData, { fetchPokemonData } from "../services/FetchData";
 import { useParams } from "react-router-dom";
 import styles from "../styles/pokePage.module.css";
+import { Context } from "../Context";
+import axios from "axios";
+
+// numero dex nacional
 
 const PokeDetails = () => {
   const { pokemonId } = useParams();
-  const [{ data, isLoading, isError }, doFetch] = FetchData(
+  const { isError, isLoading, setIsError, setIsLoading } = useContext(Context);
+
+  const [{ data }] = FetchData(
     `https://pokeapi.co/api/v2/pokemon/${pokemonId}`
   );
   const noPokemon = Object.keys(data).length == 0;
 
+  const [species, setSpecies] = useState([]);
+  const [evolution, setEvolution] = useState([]);
+
+  const capitalize = (str) => str.replace(/^\w/, (c) => c.toUpperCase());
+
+  const getEvolution = (evchain) => {
+    return fetch(evchain).then((data) => data.json());
+  };
+
   useEffect(() => {
-    console.log(data);
-  }, [data]);
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(
+          `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`
+        );
+        const result2 = await axios(result.data.evolution_chain.url);
+        setSpecies(result.data);
+        setEvolution(result2.config.url);
+      } catch (error) {
+        console.log(error);
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  // console.log(data);
   return (
     <>
+      {isError && <div>Pokemon Not Found!</div>}
+      {isLoading ? <div>Loading ...</div> : <></>}
       {noPokemon ? (
         <div>No Pokemon Found!</div>
       ) : (
         <div>
           <img alt="img" src={data.sprites.front_default} />
           <img alt="img" src={data.sprites.front_shiny} />
+          <p> {capitalize(data.name)}</p>
+          {/* {console.log(evolution)} */}
+          {/* <p>{species.flavor_text_entries[13].flavor_text}</p> */}
         </div>
       )}
-      <p>{data.name}</p>
     </>
   );
 };
