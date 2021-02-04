c5a7ca115dd7bdec0928ce80549f542a91cf604e
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2021-02-04 14:09:33 -0300

    MainInfo and EvolutionChain test pass

--- src/tests/components/mainInfo.test.js
+++ src/tests/components/mainInfo.test.js
@@ -6,44 +6,45 @@ import App from "../../App";
 import { ContextProvider } from "../../Context";
 import store from "../../redux";
 import MainInfo from "../../components/MainInfo";
-
+beforeEach(() => {
+  const name = "Venusaur";
+  const description =
+    "After a rainy day, the flower on its back smells stronger. The scent attracts other Pokémon.";
+  const types = [{ key: "grass" }, { key: "poison" }];
+  const pokemonId = "3";
+  const data = {
+    forms: [
+      { name: "Venusaur", url: "https://pokeapi.co/api/v2/pokemon-form/3/" },
+    ],
+    sprites: {
+      front_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+    },
+    name: "Venusaur",
+    id: 3,
+    height: 3,
+    weight: 30,
+  };
+  render(
+    <Provider store={store}>
+      <ContextProvider>
+        <MemoryRouter initialEntries={["/3"]} initialIndex={1}>
+          <App>
+            <MainInfo
+              data={data}
+              name={name}
+              description={description}
+              types={types}
+              pokemonId={pokemonId}
+            />
+          </App>
+        </MemoryRouter>
+      </ContextProvider>
+    </Provider>
+  );
+});
 describe("MainPage testing", () => {
   test("Should Render Venusaur page at '/3' ", async () => {
-    const name = "Venusaur";
-    const description =
-      "After a rainy day, the flower on its back smells stronger. The scent attracts other Pokémon.";
-    const types = [{ key: "grass" }, { key: "poison" }];
-    const pokemonId = "3";
-    const data = {
-      forms: [
-        { name: "Venusaur", url: "https://pokeapi.co/api/v2/pokemon-form/3/" },
-      ],
-      sprites: {
-        front_default:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-      },
-      name: "Venusaur",
-      id: 3,
-      height: 3,
-      weight: 30,
-    };
-    render(
-      <Provider store={store}>
-        <ContextProvider>
-          <MemoryRouter initialEntries={["/3"]} initialIndex={1}>
-            <App>
-              <MainInfo
-                data={data}
-                name={name}
-                description={description}
-                types={types}
-                pokemonId={pokemonId}
-              />
-            </App>
-          </MemoryRouter>
-        </ContextProvider>
-      </Provider>
-    );
     await waitFor(() => {
       expect(screen.getByText("Venusaur")).toBeInTheDocument();
     });
